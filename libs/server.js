df2c5eeca2e65e3473305102cc6d1b505a146cc6
Frendy-Vaio <frendytrojan@gmail.com>
2016-09-26 16:04:23 +0700

    astroport gates with test

--- libs/server.js
+++ libs/server.js
@@ -28,5 +28,8 @@ server.get('/minesweeper', function(request, response){
 server.get('/astroport', function (request, response) {
     require('./world.astroport/astroport')(request, response);
 });
+server.get('/astroport/gates', function (request, response) {
+    require('./world.astroport/gates')(request, response);
+});
 
 module.exports = server;
