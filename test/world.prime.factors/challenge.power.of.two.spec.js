798cadd19a7a61559b4b362c9d66421a3151bb12
bachtiar <yayan.bachtiar@ralali.com>
2016-09-26 15:39:04 +0700

    string guard

--- test/world.prime.factors/challenge.power.of.two.spec.js
+++ test/world.prime.factors/challenge.power.of.two.spec.js
@@ -1,30 +1,55 @@
 var request = require('request');
 var http    = require('http');
 var server  = require('../../libs/server');
+var expect = require('expect');
 
 describe('Passing the Power of Two level:', function() {
 
     var testServer;
-    
+
     beforeEach(function(done) {
         testServer = http.createServer(server).listen(7000, done);
     });
 
     afterEach(function() {
-        testServer.close(); 
+        testServer.close();
     });
-   
+
     it('returns the expected output', function(done) {
         request('http://localhost:7000/primeFactors?number=16', function(error, response, body) {
-            expect(body).toEqual( JSON.stringify( 
+            expect(body.replace(/\r?\n|\r|\s/g,"")).toEqual( JSON.stringify(
                 {
                     "number" : 16,
                     "decomposition" : [ 2, 2, 2, 2 ]
-                } 
+                }
             ));
-            
-            done(); 
+
+            done();
         });
     });
-   
+});
+describe('description', () => {
+
+    beforeEach(function(done) {
+        testServer = http.createServer(server).listen(7000, done);
+    });
+
+    afterEach(function() {
+        testServer.close();
+    });
+
+    it('return error not a number', done => {
+        request('http://localhost:7000/primeFactors?number=hello', function(error, response, body){
+            expect(body.replace(/\r?\n|\r/g,"")).toEqual(
+                JSON.stringify(
+                   {
+                       "number" : "hello",
+                       "error" : "not a number"
+                   }
+               )
+            );
+            done();
+        })
+    });
+
 });
