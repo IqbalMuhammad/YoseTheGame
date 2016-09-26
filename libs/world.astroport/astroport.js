9798ca7bcc217cc6b4185295761b2b419918bdb2
Frendy-Vaio <frendytrojan@gmail.com>
2016-09-26 15:43:15 +0700

    astroport testing

--- libs/world.astroport/astroport.js
+++ libs/world.astroport/astroport.js
@@ -1,15 +1,6 @@
 var astroport = function(request, response) {
-	var content="<html><head><style>";
-	content+='td { border: 1px solid #000; width: 30px; height: 30px; }\n';
-	content+='.lost{ background: red; }';
-	content+='</style>';
-	content+='</head>';
-	content+='<body><h1 id="astroport-name">Darth Vader</h1>';
-
-	content+="</body></html>";
-
 	response.setHeader('Content-Type', 'text/html');
-	response.send(content);
+	response.render('world.astroport/astroport_name.html');
 };
 
 module.exports = astroport;
