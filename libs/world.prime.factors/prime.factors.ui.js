3e2a78eb9d987bdc673a1d0c73d8187e053372f4
bachtiar <yayan.bachtiar@ralali.com>
2016-09-27 16:05:50 +0700

    complete inoput prime

--- libs/world.prime.factors/prime.factors.ui.js
+++ libs/world.prime.factors/prime.factors.ui.js
@@ -4,7 +4,12 @@ var ui = function(request, response){
     response.setHeader('Content-Type', 'text/html');
     var number = request.query['number'];
     var results =  primeFactors.doElection(number);
-    var results = results.number+ '= '+ results.decomposition;
+    var numbers = results.decomposition;
+    // console.log();
+    numbers = numbers.toString();
+    console.log(numbers);
+    numbers = numbers.replace(/[,]+/g, " x ");
+    var results = results.number+ '= '+ numbers;
     console.log(results);
     response.render("primeFactors.ui.ejs", {result:results});
 
